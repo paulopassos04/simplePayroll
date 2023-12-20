949150deff65d3e34ad42830ec09f35561935b0b
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2023-12-20 17:54:53 -0300

    criação do projeto

---
+++ src/main/java/br/com/simplePayroll/SimplePayrollApplication.java
@@ -0,0 +1,13 @@
+package br.com.simplePayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/simplePayroll/SimplePayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simplePayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
