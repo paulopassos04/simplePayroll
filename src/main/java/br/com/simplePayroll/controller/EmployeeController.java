69bfa3796df6ea839544aca19c1c86388f3535fc
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2024-03-04 08:14:17 -0300

    atualização da documentação da api com o swagger

--- src/main/java/br/com/simplePayroll/controller/EmployeeController.java
+++ src/main/java/br/com/simplePayroll/controller/EmployeeController.java
@@ -1,13 +1,13 @@
 package br.com.simplePayroll.controller;
 
 import java.util.List;
-
 import br.com.simplePayroll.domain.employee_position.Job;
 import br.com.simplePayroll.repository.EmployeeRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import br.com.simplePayroll.domain.employee.Employee;
 import br.com.simplePayroll.service.interfaces.EmployeeService;
 
+@Tag(name = "Empregado", description = "Contem todas as operações do empregado CRUD")
 @RestController
 @RequestMapping(value = "/api/v1/employee")
 public class EmployeeController {
@@ -23,9 +24,9 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-
-    @Operation(summary = "Recuperar um empregado pelo nome", description = "", responses = {
-            @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso", content = @Content(mediaType = "application/json"))
+    @Operation(summary = "Busca por nome", description = "Busca do funcionário da base de dados usando o nome", responses = {
+            @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Funcionário(a) não encontrado(a)", content = @Content(mediaType = "application/json"))
     })
     @GetMapping("/{name}")
     public ResponseEntity<List<Employee>> findByName(@PathVariable("name") String name) {
@@ -34,18 +35,30 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 
+    @Operation(summary = "Listar funcionários", description = "Listar funcionários da base de dados usando por cargo", responses = {
+            @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Cargo não encontrado", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/job/{id}")
     public ResponseEntity<List<Employee>> findByJob(@PathVariable("id") Long id) {
         List<Employee> employees = employeeService.findByJobId(id);
         return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 
+    @Operation(summary = "Atualizar funcionário", description = "Atualizar funcionário da base de dados", responses = {
+            @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Funcionário(a) não encontrado(a)", content = @Content(mediaType = "application/json"))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Employee> update(@PathVariable("id") Long id, @RequestBody Employee employee) {
         employeeService.update(id, employee);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(summary = "Deletar funcionário", description = "Deletar funcionário da base de dados", responses = {
+            @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Funcionário(a) não encontrado(a)", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Employee> delete(@PathVariable("id") Long id) {
         employeeService.delete(id);
